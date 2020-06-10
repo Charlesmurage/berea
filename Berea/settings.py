496bee814eb5627600934bb1638488d68cf5922c
Charles maina <muragemaina09@gmail.com>
2020-06-10 12:28:09 +0300

    added static

--- Berea/settings.py
+++ Berea/settings.py
@@ -145,7 +145,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATIC_URL = '/static/'
+STATIC_URL = 'website/static'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
